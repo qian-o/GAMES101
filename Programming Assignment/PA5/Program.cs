4d85c9f9de34fd389ca7b451f251ca35c934a3f3
王先生 <1324771795@qq.com>
2024-06-18 00:56:43 +0800

    添加新项目PA5及其配置和依赖
    在 `GAMES101.sln` 解决方案中，我们进行了几项重要的更新。首先，添加了一个名为 `PA5` 的新项目，并为其配置了 Debug 和 Release 模式。这一步骤确保了新项目可以在不同的编译环境下正确构建。接着，我们为 `PA5` 项目设置了编译配置，包括激活配置和构建配置，以确保项目在不同模式下的正确编译。
    
    此外，`PA5` 项目被添加到了一个现有的项目组中，这样做是为了在解决方案资源管理器中以分组的形式更好地组织和显示项目。这有助于提高项目的可管理性和可访问性。
    
    在 `PA.csproj` 项目文件中，我们添加了多个新的包引用，这些包引用是为了支持项目中的图形和输入处理功能。引入的包包括 `Silk.NET.Input`、`Silk.NET.OpenGL`、`Silk.NET.OpenGL.Extensions.ImGui`、`Silk.NET.Windowing` 和 `SkiaSharp`。这些包的添加极大地丰富了项目的功能，使其能够处理更复杂的图形和输入事件。
    
    创建了一个新的 `PA5.csproj` 项目文件，为控制台应用程序配置了基本的项目设置，这包括输出类型、目标框架、隐式使用、可空引用类型和允许不安全代码块的配置。此外，还添加了对 `PA` 项目的引用，确保了项目间的依赖关系正确设置。
    
    最后，在 `PA5` 项目中添加了一个新的 `Program.cs` 文件，其中定义了 `PA5` 命名空间下的 `Program` 类。在其 `Main` 方法中，创建了一个 `WindowRenderer` 实例，负责运行和管理窗口渲染。应用程序执行完成后，还负责释放 `WindowRenderer` 实例的资源，确保了资源的正确管理和释放。

---
+++ Programming Assignment/PA5/Program.cs
@@ -0,0 +1,17 @@
+﻿using PA.Graphics;
+
+namespace PA5;
+
+internal class Program
+{
+    private static WindowRenderer _windowRenderer = null!;
+
+    static void Main(string[] _)
+    {
+        _windowRenderer = new("PA 5");
+
+        _windowRenderer.Run();
+
+        _windowRenderer.Dispose();
+    }
+}
