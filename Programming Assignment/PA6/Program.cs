c1bcf508df41a98280a587e1cae8e235ed3239cd
王先生 <1324771795@qq.com>
2024-06-23 17:18:57 +0800

    添加新项目PA6及其配置和基础代码
    在解决方案 `GAMES101.sln` 中添加了新的项目 `PA6`，并为其配置了编译设置。此项目使用 `Microsoft.NET.Sdk`，目标框架为 `.NET 8.0`，启用了隐式命名空间、可空引用类型和不安全代码块，增强了项目的现代化特性和安全性。同时，`PA6` 项目被添加到了指定的解决方案文件夹中，以保持解决方案的组织结构清晰。此外，`PA6` 项目中新增了 `Program.cs` 文件，实现了窗口渲染器的基础逻辑，为后续的开发奠定了基础。

---
+++ Programming Assignment/PA6/Program.cs
@@ -0,0 +1,28 @@
+﻿using ImGuiNET;
+using PA.Graphics;
+
+namespace PA6;
+
+
+internal class Program
+{
+    private static WindowRenderer _windowRenderer = null!;
+
+    static void Main(string[] _)
+    {
+        _windowRenderer = new("PA 6");
+        _windowRenderer.Render += WindowRenderer_Render;
+
+        _windowRenderer.Run();
+
+        _windowRenderer.Dispose();
+    }
+
+    private static void WindowRenderer_Render(float obj)
+    {
+        ImGui.Begin("PA 6");
+        {
+        }
+        ImGui.End();
+    }
+}
